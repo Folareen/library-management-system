bbefba7213923d7e6d5fa453eb887fdd96064ff4
Folareen <sakawahab03@gmail.com>
2022-09-20 01:10:17 +0100

    main server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
 
 const app = express();
 const PORT = 4000;
@@ -11,6 +12,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use("/users", usersRouter);
+app.use("/books", booksRouter);
 
 app.get("/", (req, res) => {
   res.render("index");
